94ed8cf1681b8186300fa962080cd7e49d56f12b
Anton Yeryomin <qrbg1251@gmail.com>
2018-09-29 23:30:52 +0700

    Add divider to list

--- app/src/main/java/ntktest/example/com/ntktest/presentation/FeedFragment.java
+++ app/src/main/java/ntktest/example/com/ntktest/presentation/FeedFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -55,6 +56,8 @@ public class FeedFragment extends Fragment {
         recycler.setAdapter(adapter);
 
         recycler.setHasFixedSize(true);
+        recycler.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
     }
 
     @Override
