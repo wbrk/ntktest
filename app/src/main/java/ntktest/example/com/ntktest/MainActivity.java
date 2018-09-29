786f7de4946a1c207efca5c2d87cf05bcdd6c6a0
Anton Yeryomin <qrbg1251@gmail.com>
2018-09-29 17:39:34 +0700

    Convert response to RssDocument

--- app/src/main/java/ntktest/example/com/ntktest/MainActivity.java
+++ app/src/main/java/ntktest/example/com/ntktest/MainActivity.java
@@ -4,13 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.io.IOException;
+import org.simpleframework.xml.convert.AnnotationStrategy;
+import org.simpleframework.xml.core.Persister;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -19,29 +20,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.reload).setOnClickListener(v -> load());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        load();
+    }
+
+    private void load() {
         Retrofit r = new Retrofit.Builder()
-                .baseUrl("https://meduza.io/rss/all/")
+                .baseUrl("https://example.com")
+                .addConverterFactory(SimpleXmlConverterFactory.createNonStrict(new Persister(new AnnotationStrategy())))
                 .build();
 
+        String url = "https://meduza.io/rss/all/";
         RssService s = r.create(RssService.class);
-        Call<ResponseBody> data = s.getData("https://meduza.io/rss/all/");
-        data.enqueue(new Callback<ResponseBody>() {
+        Call<RssDocument> data = s.getData(url);
+        data.enqueue(new Callback<RssDocument>() {
             @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                try {
-                    Log.e(TAG, "onResponse: length = " + response.body().string().length());
-                } catch (IOException e) {
-                    Log.e(TAG, "onResponse: error: " + e);
+            public void onResponse(Call<RssDocument> call, Response<RssDocument> response) {
+                RssDocument d = response.body();
+                Log.e(TAG, "onResponse: " + d);
+                for (RssItem i : d.getItems()) {
+                    Log.e(TAG, "onResponse: " + i);
                 }
             }
 
             @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
+            public void onFailure(Call<RssDocument> call, Throwable t) {
                 Log.e(TAG, "onFailure: error: " + t);
             }
         });
--- app/src/main/java/ntktest/example/com/ntktest/RssService.java
+++ app/src/main/java/ntktest/example/com/ntktest/RssService.java
@@ -1,11 +1,10 @@
 package ntktest.example.com.ntktest;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Url;
 
 public interface RssService {
     @GET
-    Call<ResponseBody> getData(@Url String url);
+    Call<RssDocument> getData(@Url String url);
 }
