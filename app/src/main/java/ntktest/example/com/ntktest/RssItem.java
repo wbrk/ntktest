457eb42b6a8a3c0c51286b54bea135b146397664
Anton Yeryomin <qrbg1251@gmail.com>
2018-09-29 17:35:58 +0700

    Add rss data classes

---
+++ app/src/main/java/ntktest/example/com/ntktest/RssItem.java
@@ -0,0 +1,52 @@
+package ntktest.example.com.ntktest;
+
+import android.support.annotation.NonNull;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+@Root(strict = false, name = "item")
+public class RssItem {
+    @NonNull
+    @Element(required = false, data = true)
+    private String title = "";
+
+    @NonNull
+    @Element(required = false, data = true)
+    private String description = "";
+
+    @NonNull
+    @Element(required = false)
+    private String link = "";
+
+    @NonNull
+    @Element(required = false)
+    private String pubDate = "";
+
+    @NonNull
+    public String getTitle() {
+        return title;
+    }
+
+    @NonNull
+    public String getLink() {
+        return link;
+    }
+
+    @NonNull
+    public String getDescription() {
+        return description;
+    }
+
+    @NonNull
+    public String pubDate() {
+        return pubDate;
+    }
+
+    @NonNull
+    public String toString() {
+        return "RssItem { "
+                + "title=" + title + ", "
+                + "link=" + link + " }";
+    }
+}
