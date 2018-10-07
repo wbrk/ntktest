c0f6eda00266822519f1d9b83ba3914a96985938
Anton Yeryomin <qrbg1251@gmail.com>
2018-10-07 22:51:04 +0700

    Add source fragment

---
+++ app/src/main/java/ntktest/example/com/ntktest/presentation/source/SourceFragment.java
@@ -0,0 +1,40 @@
+package ntktest.example.com.ntktest.presentation.source;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import ntktest.example.com.ntktest.R;
+
+public class SourceFragment extends Fragment {
+    private static final String TAG = "SourceFragment";
+
+    @BindView(R.id.list)
+    RecyclerView recycler;
+
+    private Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_source, container, false);
+        unbinder = ButterKnife.bind(this, v);
+        return v;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
