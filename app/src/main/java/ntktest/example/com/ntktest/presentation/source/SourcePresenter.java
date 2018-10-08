b949f9c64a89ea66d86a8adc25b1065e57ef7a99
Anton Yeryomin <qrbg1251@gmail.com>
2018-10-08 23:55:48 +0700

    Implement all the logic behind add/edit screen

--- app/src/main/java/ntktest/example/com/ntktest/presentation/source/SourcePresenter.java
+++ app/src/main/java/ntktest/example/com/ntktest/presentation/source/SourcePresenter.java
@@ -2,11 +2,31 @@ package ntktest.example.com.ntktest.presentation.source;
 
 import android.support.annotation.NonNull;
 
+import ntktest.example.com.ntktest.data.RssSource;
+import ntktest.example.com.ntktest.data.RssSourceRepository;
+
 public class SourcePresenter {
     @NonNull
     private SourceView view;
 
     public SourcePresenter(@NonNull SourceView view) {
         this.view = view;
+    }
+
+    @NonNull
+    public RssSource getById(int id) {
+        return RssSourceRepository.instance().getById(id);
+    }
+
+    public void add(@NonNull RssSource source) {
+        RssSourceRepository.instance().add(source);
+    }
+
+    public void update(@NonNull RssSource source) {
+        RssSourceRepository.instance().update(source);
+    }
+
+    public void delete(@NonNull RssSource source) {
+        RssSourceRepository.instance().delete(source);
     }
 }
