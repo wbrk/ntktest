e43c876a1bc63ef0c030e43c3bf76291a2f1628d
Anton Yeryomin <qrbg1251@gmail.com>
2019-07-13 18:24:42 +0700

    Apply MaterialComponents theme

--- app/src/main/java/com/example/rssreader/data/RssItemConverter.kt
+++ app/src/main/java/com/example/rssreader/data/RssItemConverter.kt
@@ -3,7 +3,6 @@ package com.example.rssreader.data
 import com.example.rssreader.data.model.RssItemModel
 import com.example.rssreader.domain.entity.RssItem
 
-// todo rewrite as a function?
 object RssItemConverter : Converter<RssItemModel, RssItem> {
     override fun convert(value: RssItemModel): RssItem =
         RssItem(
