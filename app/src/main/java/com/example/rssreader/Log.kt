1a4402f7c13b97458450a11dfbc53f7c04d1cb71
Anton Yeryomin <qrbg1251@gmail.com>
2019-07-13 13:03:57 +0700

    Fix logging functions causing wrong tags in logcat output

--- app/src/main/java/com/example/rssreader/Log.kt
+++ app/src/main/java/com/example/rssreader/Log.kt
@@ -3,17 +3,17 @@ package com.example.rssreader
 import timber.log.Timber
 
 // region error
-fun logError(t: Throwable) = Timber.e(t)
+inline fun logError(t: Throwable) = Timber.e(t)
 
-fun logError(message: String, vararg args: Any) = Timber.e(message, args)
+inline fun logError(message: String, vararg args: Any) = Timber.e(message, args)
 
-fun logError(t: Throwable, message: String, vararg args: Any) = Timber.e(t, message, args)
+inline fun logError(t: Throwable, message: String, vararg args: Any) = Timber.e(t, message, args)
 // endregion error
 
 // region info
-fun logInfo(t: Throwable) = Timber.e(t)
+inline fun logInfo(t: Throwable) = Timber.e(t)
 
-fun logInfo(message: String, vararg args: Any) = Timber.e(message, args)
+inline fun logInfo(message: String, vararg args: Any) = Timber.e(message, args)
 
-fun logInfo(t: Throwable, message: String, vararg args: Any) = Timber.e(t, message, args)
+inline fun logInfo(t: Throwable, message: String, vararg args: Any) = Timber.e(t, message, args)
 // endregion info
